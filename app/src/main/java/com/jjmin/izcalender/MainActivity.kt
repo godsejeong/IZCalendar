3cac0c6de9af91a19e02de1068304515098dd1eb
godsejeong <dbdnpxm@naver.com>
2019-03-20 14:22:34 +0900

    [add] develop Pasing system

--- app/src/main/java/com/jjmin/izcalender/MainActivity.kt
+++ app/src/main/java/com/jjmin/izcalender/MainActivity.kt
@@ -5,19 +5,25 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.RecyclerView.LayoutManager
+import android.util.Log
 import android.widget.Toast
 import com.github.nitrico.lastadapter.LastAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var list = ArrayList<Any>()
+    var planningInfo = PlanningModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.e("Oncreate","T")
+
+        planInfo()
+
         mainRecycler.layoutManager = LinearLayoutManager(this)
 
-        val into = LastAdapter(list,BR.item)
+        LastAdapter(list,BR.item)
             .map<PlanningData, com.jjmin.izcalender.databinding.ItemPlanningBinding>(R.layout.item_planning)
             {
                 onClick {
@@ -26,4 +32,10 @@ class MainActivity : AppCompatActivity() {
             }
             .into(mainRecycler)
     }
+
+    fun planInfo(){
+        planningInfo.execute()
+        list.addAll(planningInfo.infoList)
+    }
+
 }
--- app/src/main/java/com/jjmin/izcalender/PlanningModel.kt
+++ app/src/main/java/com/jjmin/izcalender/PlanningModel.kt
@@ -1,13 +1,34 @@
 package com.jjmin.izcalender
 
-class PlanningModel : Thread() {
+import android.os.AsyncTask
+import android.util.Log
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
-    var url = "http://m.cafe.daum.net/official-izone/l0C7?boardType=Q"
+class PlanningModel : AsyncTask<String,Void,ArrayList<PlanningData>>() {
 
-    override fun run() {
-        super.run()
+    private var url = "http://m.cafe.daum.net/official-izone/l0C7?boardType=Q"//ㅈ같은 권한 씨발
+    private var doc : Document?= null
+    var infoList = ArrayList<PlanningData>()
 
 
+    override fun doInBackground(vararg params: String?): ArrayList<PlanningData>? {
+        doc = Jsoup.connect(url).get()
+        print(doc)
+        var titleTag = doc?.select(".tit_subject")
+        var subTag = doc?.select("em")
+        var timeTag = doc?.select("span.inner_tit")
 
+        Log.e("titleTag", titleTag!!.size.toString())
+        titleTag?.forEach {
+            Log.e("data","${titleTag.text()}   ${subTag!!.text()}    ${timeTag!!.text()}")
+            infoList.add(PlanningData(titleTag.text(),subTag!!.text(),timeTag!!.text()))
+        }
+
+        infoList.forEach {
+            Log.e(it.title,it.subtitle + it.time)
+        }
+
+        return infoList
     }
 }
