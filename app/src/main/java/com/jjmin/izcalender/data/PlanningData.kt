5f659a3ea9bc2b48c02570da13cfbcdb24b4017c
godsejeong <dbdnpxm@naver.com>
2019-04-01 21:38:51 +0900

    git mainlayout update

--- app/src/main/java/com/jjmin/izcalender/data/PlanningData.kt
+++ app/src/main/java/com/jjmin/izcalender/data/PlanningData.kt
@@ -5,5 +5,4 @@ import android.databinding.BindingAdapter
 import android.util.TypedValue
 import android.widget.TextView
 
-
 class PlanningData(var title : String?,var subtitle : String?,var time : String?,var day : String?,var dow : String?)
--- app/src/main/java/com/jjmin/izcalender/view/MainActivity.kt
+++ app/src/main/java/com/jjmin/izcalender/view/MainActivity.kt
@@ -1,61 +1,108 @@
 package com.jjmin.izcalender.view
 
-import android.arch.lifecycle.Transformations.map
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.util.TypedValue
-import android.view.LayoutInflater
-import android.view.View
+import android.view.*
 import android.widget.Toast
 import com.github.nitrico.lastadapter.*
-import com.github.nitrico.lastadapter.BR.item
 import com.jjmin.izcalender.BR
 import com.jjmin.izcalender.data.PlanningData
 import com.jjmin.izcalender.model.PlanningModel
-import com.jjmin.izcalender.util.Utils
 import kotlinx.android.synthetic.main.activity_main.*
-import com.jjmin.izcalender.R
 import com.jjmin.izcalender.databinding.ItemPlanningBinding
-import com.jjmin.izcalender.databinding.ItemPlanningDayBinding
-import kotlinx.android.synthetic.main.item_planning.view.*
-import org.jetbrains.annotations.NotNull
+import android.view.View.MeasureSpec
+import android.util.DisplayMetrics
+import com.jjmin.izcalender.R
+import android.graphics.Rect
 
 
 class MainActivity : AppCompatActivity() {
     var list = ArrayList<Any>()
     var planningInfo = PlanningModel()
-
+    var MAXHEIGHT = 90
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.e("Oncreate", "T")
         planInfo()
+        todayView.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
 
+
+
+//        var detector = GestureDetector(this, object : GestureDetector.OnGestureListener {
+//            override fun onShowPress(e: MotionEvent?) {
+//            }
+//
+//            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+//                return true
+//            }
+//
+//            override fun onDown(e: MotionEvent?): Boolean {
+//                return true
+//            }
+//
+//            override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+//
+//                return true
+//            }
+//
+//            override fun onLongPress(e: MotionEvent?) {
+//            }
+//
+//
+//            override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+//                Log.e("onScroll 호출됨", "${pxToDp(todayView.height)},${todayView.height - (distanceY)}")
+//
+//
+//                val params = todayView.layoutParams
+////                when {
+////                    pxToDp(todayView.height) in 12..MAXHEIGHT -> {
+////                        Log.e("todayView", pxToDp(todayView.height).toString())
+////                        params.height = (todayView.height - (distanceY).toInt())
+////                    }
+////
+////                    pxToDp(todayView.height) == 11 -> {
+////                        params.height = (todayView.height - (distanceY).toInt())
+////                    }
+////                }
+//
+//                todayView.layoutParams = params
+//                return true
+//            }
+//        })
+//
+//        todayScrollbar.setOnTouchListener { v, event ->
+//            detector.onTouchEvent(event)
+//        }
 
         mainRecycler.layoutManager = LinearLayoutManager(this)
 
-        LastAdapter(list,BR.item)
-            .map<PlanningData,ItemPlanningBinding>(R.layout.item_planning)
+        LastAdapter(list, BR.item)
+            .map<PlanningData, ItemPlanningBinding>(R.layout.item_planning)
             {
                 onBind {
                     it.binding.run {
                         item?.let { data ->
-                            if(data.time!!.length >= 6){
+                            if (data.time!!.length >= 6) {
                                 planTime.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13.72F)
                             }
                         }
                     }
                 }
                 onClick {
-                    Toast.makeText(applicationContext,"Click",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Click", Toast.LENGTH_SHORT).show()
                 }
             }
             .into(mainRecycler)
+    }
+
+    fun pxToDp(px: Int): Int {
+        val displayMetrics = application.resources.displayMetrics
+        return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
     }
 
     fun planInfo() {
